86c16a88f9d3a80a56169ed0c829e1dc77188c80
KEEN <kyh0927@live.co.kr>
2018-11-15 00:41:24 +0900

    Blog Front-end #1

---
+++ src/pages/ListPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PageTemplate from 'components/common/PageTemplate';
+import ListWrapper from 'components/list/ListWrapper';
+import PostList from 'components/list/PostList';
+
+const ListPage = () => {
+  return (
+    <PageTemplate>
+      <ListWrapper>
+        <PostList/>
+      </ListWrapper>
+    </PageTemplate>
+  );
+};
+
+export default ListPage;
